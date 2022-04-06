967d80230f5c299cadea6f5819a2ec2c3e763526
Saqib Motlani <motlani.saqib@gmail.com>
2022-04-06 18:54:03 -0500

    updated requirements.txt

--- symport/settings/prod.py
+++ symport/settings/prod.py
@@ -1,11 +1,12 @@
 import os
-
+import dj_database_url
 from symport.settings.dev import SECRET_KEY
 from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
