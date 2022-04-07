1dcbad4484aa650dcb175dd7f350b91c95ac42ff
Saqib Motlani <motlani.saqib@gmail.com>
2022-04-06 19:59:51 -0500

    added database to prod setting

--- symport/settings/prod.py
+++ symport/settings/prod.py
@@ -1,12 +1,14 @@
 import os
+import dj_database_url
 from symport.settings.dev import SECRET_KEY
 from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES = {
+    'default': dj_database_url.config()
+    
+}
 SECRET_KEY = os.environ['SECRET_KEY']
 
 GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
