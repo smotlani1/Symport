e75b7d0409104619fcb200e21c9777313a935dfa
Saqib Motlani <motlani.saqib@gmail.com>
2022-04-14 09:46:01 -0500

    remove dependancy on dev file

--- symport/settings/prod.py
+++ symport/settings/prod.py
@@ -1,6 +1,6 @@
 import os
 import dj_database_url
-from symport.settings.dev import SECRET_KEY
+# from symport.settings.dev import SECRET_KEY
 from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
