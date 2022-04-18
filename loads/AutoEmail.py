87833d45a686773774809b4a93d104cb314bb460
Saqib Motlani <motlani.saqib@gmail.com>
2022-04-18 13:32:41 -0500

    add s3 files to autoemail.py

--- loads/AutoEmail.py
+++ loads/AutoEmail.py
@@ -1,8 +1,10 @@
-# import os
-# import smtplib
-# from email.message import EmailMessage
-# from django.conf import settings
+import os
+import smtplib
+from email.message import EmailMessage
+from django.conf import settings
+import boto3
 
+s3 = boto3.client('s3',  aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY, region_name='us-east-1')
 # #This function automatically sends emails to customers attached with the corresponding invoice and documents
 
 
@@ -21,41 +23,46 @@
 
 
 
-# class Email:
+class Email:
 
 #     #Add all files in a given directory to a dictionary as a key and store file-type as value
 #     #Allows for proper attachment when calling add_attachment method of the EmailMessage Class
 
-#     def get_files(self, files_dir):
-#         file_dict = {}
-#         for dir, folders, files in os.walk(files_dir):
-#             for file in files:
-#                 file_path = dir + file #generate separate file path in dictionary for each individual file to be attached
-#                 file_type = os.path.splitext(file)[1]
-#                 file_dict[file_path] = file_type
-#         print(file_dict)
-#         return file_dict
+    # def get_files(self, files_dir):
+    #     file_dict = {}
+    #     for dir, folders, files in os.walk(files_dir):
+    #         for file in files:
+    #             file_path = dir + file #generate separate file path in dictionary for each individual file to be attached
+    #             file_type = os.path.splitext(file)[1]
+    #             file_dict[file_path] = file_type
+    #     print(file_dict)
+    #     return file_dict
 
 #     #send_email generates the complete email with from, to, subject line, and attachments, and sends it
-#     def send_email(self, file_dict, load_ref):
+    def send_email(self, file_dict, load_ref):
         
-#         msg = EmailMessage()
-#         msg ['Subject'] = "Invoice and POD for Load " + load_ref
-#         msg['From'] = email_address
-#         msg['To'] = 'motlani.saqib@gmail.com'
+        msg = EmailMessage()
+        msg ['Subject'] = "Invoice and POD for Load " + load_ref
+        msg['From'] = settings.EMAIL_USER
+        msg['To'] = 'motlani.saqib@gmail.com'
 
-#         for file, path in file_dict.items():
-#             with open(file, 'rb') as f:
-#                 file_data = f.read()
-#                 file_name = f.name
+        s3_data = s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key='Symport_Invoice_Template.xlsx')
+        contents = s3_data['Body'].read()
+   
+        msg.add_attachment(contents, maintype='application', subtype='xlsx', filename=load_ref + "_Invoice.xlsx")
 
-#                 if path == ".jpg":
-#                     msg.add_attachment(file_data, maintype='image', subtype='jpg', filename=load_ref + "POD")
-#                 if path == '.xlsx':
-#                     msg.add_attachment(file_data, maintype='application', subtype='xlsx', filename=load_ref + "_Invoice.xlsx")
+        # for file, path in file_dict.items():
+        #     with open(file, 'rb') as f:
+        #         file_data = f.read()
+        #         file_name = f.name
 
-#         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-#             smtp.login(email_address, email_password)
-#             smtp.send_message(msg)
+        #         if path == ".jpg":
+        #             msg.add_attachment(file_data, maintype='image', subtype='jpg', filename=load_ref + "POD")
+        #         if path == '.xlsx':
+        #             msg.add_attachment(file_data, maintype='application', subtype='xlsx', filename=load_ref + "_Invoice.xlsx")
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(settings.EMAIL_USER, settings.EMAIL_PASS)
+            smtp.send_message(msg)
 
 
